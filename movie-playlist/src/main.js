dd2bd91c86ffc5882543131900b6c435050b0b36
herrinekim <herrine.kim@gmail.com>
2022-05-22 12:55:43 +0900

    Fix profile 404 error

--- movie-playlist/src/main.js
+++ movie-playlist/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 
+import VueGeolocationApi from 'vue-geolocation-api'
 import VueCookies from 'vue-cookies'
 
 import App from './App.vue'
@@ -8,7 +9,7 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-Vue.use(VueCookies)
+Vue.use(VueGeolocationApi, VueCookies)
 
 new Vue({
   router,
