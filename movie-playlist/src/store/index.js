b83de82f94a815ca7c92792cb73d8066783e32fe
Hyerin Kim <herrine.kim@gmail.com>
2022-05-23 22:02:13 +0900

    Add movies store

--- movie-playlist/src/store/index.js
+++ movie-playlist/src/store/index.js
@@ -5,9 +5,11 @@ import Vuex from 'vuex'
 import accounts from './modules/accounts'
 import home from './modules/home'
 import articles from './modules/articles'
+import movies from './modules/movies'
+import tags from './modules/tags'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: { accounts, home, articles },
+  modules: { accounts, home, articles, movies, tags },
 })
