d2b6aedc6e91c117ce9c7bddfefcab52c2f6cb6f
letgodchan0 <letgodchan0@gmail.com>
2022-05-23 09:25:20 +0900

    Add actor

---
+++ BE/movies/serializers/actor.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from ..models import Movie, Actor
+
+# 배우 목록 정보
+class ActorListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+# 단일 배우 상세 정보
+class ActorSerializer(serializers.ModelSerializer):
+
+    class MovieSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Movie
+            fields = ('title')
+
+    movie = MovieSerializer(Movie, read_only=True)
+
+    class Meta:
+        model = Actor
+        fields = '__all__'
--- BE/movies/views.py
+++ BE/movies/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404
+
+from .serializers.actor import ActorListSerializer, ActorSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import Actor
 
 # Create your views here.
+@api_view(['GET'])
+def actor_list(request):
+    actors = get_list_or_404(Actor)
+    serializer = ActorListSerializer(actors, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def actor_detail(request, actor_pk):
+    actor = get_object_or_404(Actor, pk=actor_pk)
+    serializer = ActorSerializer(actor)
+    return Response(serializer.data)
